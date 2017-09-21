a81ef967bed331387f3d8f10a634366d3a533fdc
JDTorresP <jd.torres11@uniandes.edu.co>
2017-09-21 09:12:53 -0500

    done

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {cyan500} from 'material-ui/styles/colors';
+import AppBar from 'material-ui/AppBar';
+
+
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: cyan500,
+  },
+  appBar: {
+    height: 50,
+  },
+});
 
 class App extends Component {
 
- componentDidMount(){
-        fetch("/getFollowers/john-guerra")
-        .then((res)=> res.json())
-        .then((followers) => {
-           console.log(followers)
-        });
-    }
+  componentDidMount() {
+    fetch("/getFollowers/john-guerra").then((res) => res.json()).then((followers) => {
+      console.log(followers)
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <MuiThemeProvider  muiTheme={muiTheme}>
+          <AppBar title="Github Followers" />
+        </MuiThemeProvider>
       </div>
     );
   }
---
+++ front/src/SearchBox.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+
+class SearchBox extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+    onkeypress(){
+
+    }
+    
+
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default SearchBox;
